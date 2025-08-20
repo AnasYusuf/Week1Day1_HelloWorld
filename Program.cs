cba5f239184e84b228f0a7e9ec0eb182c88ab489
Anas Yusuf <anasyusuf6@gmail.com>
2025-08-20 13:38:18 +0800

    Initial commit - WeekDay1 HelloWorld

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("Hello, " + name + "!");
+        Console.WriteLine("Today is: " + DateTime.Now.ToString("dddd, dd MMMM yyyy"));
+    }
+}
